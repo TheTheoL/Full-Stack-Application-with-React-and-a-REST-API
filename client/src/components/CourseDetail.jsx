e47ec628f8e61953c8d21ead63eed9d07f38767b
Theo L <atlufkin@outlook.com>
2022-03-25 13:41:20 -0400

    “updated”

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,10 +1,53 @@
-import React, { Component } from 'react';
-import './Courses';
+import React from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 
+function CourseDetail() {
 
+
+
+    const [course, setCourse] = useState('')
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/courses/")
+            .then(res => setState(res.data)
+            )
+    })
+
+    return (
+        <main>
+            <div class="actions--bar">
+                <div class="wrap">
+                    <NavLink className="button" to="/updatecourse"> Update Course</NavLink>
+                    <a class="button" href="#">Delete Course</a>
+                    <a class="button button-secondary" href="index.html">Return to List</a>
+                </div>
+            </div>
+
+            <div class="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div class="main--flex">
+                        <div>
+                            <h3 class="course--detail--title">Course</h3>
+                            <h4 class="course--name">{course.title}</h4>
+                            <p>By {user.firstName} {user.lastName}</p>
+
+                            <ReactMarkdown>{course.description}</ReactMarkdown>
+                        </div>
+                        <div>
+                            <h3 class="course--detail--title">Estimated Time</h3>
+                            <p>{course.estimatedTime}</p>
+
+                            <h3 class="course--detail--title">Materials Needed</h3>
+                            <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
+}
 
 
 // export default class CourseDetail extends Component {
@@ -67,44 +110,4 @@ import ReactMarkdown from 'react-markdown';
 //     }
 // }
 
-
-function CourseDetail() {
-
-    const [course, setCourse] = useState('')
-
-
-    return (
-        <main>
-            <div class="actions--bar">
-                <div class="wrap">
-                    <NavLink className="button" to="/updatecourse"> Update Course</NavLink>
-                    <a class="button" href="#">Delete Course</a>
-                    <a class="button button-secondary" href="index.html">Return to List</a>
-                </div>
-            </div>
-
-            <div class="wrap">
-                <h2>Course Detail</h2>
-                <form>
-                    <div class="main--flex">
-                        <div>
-                            <h3 class="course--detail--title">Course</h3>
-                            <h4 class="course--name">{course.title}</h4>
-                            <p>By {user.firstName} {user.lastName}</p>
-
-                            <ReactMarkdown>{course.description}</ReactMarkdown>
-                        </div>
-                        <div>
-                            <h3 class="course--detail--title">Estimated Time</h3>
-                            <p>{course.estimatedTime}</p>
-
-                            <h3 class="course--detail--title">Materials Needed</h3>
-                            <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </main>
-    );
-}
 

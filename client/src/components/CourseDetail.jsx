afb8cf591b003ce49b21b035d75bf415d2597e08
Theo L <atlufkin@outlook.com>
2022-03-25 13:48:24 -0400

    “updated”

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';

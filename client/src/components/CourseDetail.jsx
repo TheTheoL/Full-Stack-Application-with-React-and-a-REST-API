6609ef53b8be3b71c36cd4ce336d89537fa1712c
Theo L <atlufkin@outlook.com>
2022-03-25 14:01:56 -0400

    “updated”

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -10,7 +10,7 @@ export default function CourseDetail(props) {
         axios.get("http://localhost:5000/api/courses/")
             .then(res => setState(res.data)
             )
-    })
+    }, [])
 
     return (
         <main>

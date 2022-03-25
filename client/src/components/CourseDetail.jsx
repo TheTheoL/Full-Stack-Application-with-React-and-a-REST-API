a3709f0333fa9302fc72897a85d5c1be9eb20b5c
Theo L <atlufkin@outlook.com>
2022-03-25 13:44:18 -0400

    “updated”

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -3,9 +3,7 @@ import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 
-function CourseDetail() {
-
-
+export default function CourseDetail() {
 
     const [course, setCourse] = useState('')
     useEffect(() => {
@@ -16,30 +14,30 @@ function CourseDetail() {
 
     return (
         <main>
-            <div class="actions--bar">
-                <div class="wrap">
+            <div className="actions--bar">
+                <div className="wrap">
                     <NavLink className="button" to="/updatecourse"> Update Course</NavLink>
-                    <a class="button" href="#">Delete Course</a>
-                    <a class="button button-secondary" href="index.html">Return to List</a>
+                    <a className="button" href="#">Delete Course</a>
+                    <a className="button button-secondary" href="index.html">Return to List</a>
                 </div>
             </div>
 
-            <div class="wrap">
+            <div className="wrap">
                 <h2>Course Detail</h2>
                 <form>
-                    <div class="main--flex">
+                    <div className="main--flex">
                         <div>
-                            <h3 class="course--detail--title">Course</h3>
-                            <h4 class="course--name">{course.title}</h4>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{course.title}</h4>
                             <p>By {user.firstName} {user.lastName}</p>
 
                             <ReactMarkdown>{course.description}</ReactMarkdown>
                         </div>
                         <div>
-                            <h3 class="course--detail--title">Estimated Time</h3>
+                            <h3 className="course--detail--title">Estimated Time</h3>
                             <p>{course.estimatedTime}</p>
 
-                            <h3 class="course--detail--title">Materials Needed</h3>
+                            <h3 className="course--detail--title">Materials Needed</h3>
                             <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
                         </div>
                     </div>
@@ -50,64 +48,6 @@ function CourseDetail() {
 }
 
 
-// export default class CourseDetail extends Component {
 
-
-
-//     componentDidMount({ match }) {
-//         axios.get(`http://localhost:5000/api/courses/${match.path}`)
-//             .then(res => {
-//                 this.setState({
-//                     courses: res.data,
-//                     users: res.data
-//                 })
-//             })
-//             .catch(error => {
-//                 console.log('Error fetching and parsing data', error);
-//             });
-//     }
-
-
-//     render() {
-//         return (
-//             // console.log(this.props)
-
-//             <main>
-//                 <div class="actions--bar">
-//                     <div class="wrap">
-//                         <NavLink className="button" to="/updatecourse"> Update Course</NavLink>
-//                         <a class="button" href="#">Delete Course</a>
-//                         <a class="button button-secondary" href="index.html">Return to List</a>
-//                     </div>
-//                 </div>
-
-//                 <div class="wrap">
-//                     <h2>Course Detail</h2>
-//                     <form>
-//                         <div class="main--flex">
-//                             <div>
-//                                 <h3 class="course--detail--title">Course</h3>
-//                                 <h4 class="course--name">{course.title}</h4>
-//                                 <p>By {user.firstName} {user.lastName}</p>
-
-//                                 <ReactMarkdown>{course.description}</ReactMarkdown>
-//                             </div>
-//                             <div>
-//                                 <h3 class="course--detail--title">Estimated Time</h3>
-//                                 <p>{course.estimatedTime}</p>
-
-//                                 <h3 class="course--detail--title">Materials Needed</h3>
-//                                 <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
-//                             </div>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </main>
-
-
-
-//         );
-//     }
-// }
 
 

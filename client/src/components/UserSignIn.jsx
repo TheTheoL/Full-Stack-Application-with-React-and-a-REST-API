35d94cc145ae91b6c2ea13c54b6d3475715a1ef3
Theo L <atlufkin@outlook.com>
2022-03-26 10:37:34 -0400

    “updated”

--- client/src/components/UserSignIn.jsx
+++ client/src/components/UserSignIn.jsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react'
 
 export default function UserSignIn() {
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        submit();
+    }
+
+    function handleCancel(event) {
+        event.preventDefault();
+        cancel();
+    }
+
     return (
         <div className="form--centered">
             <h2>Sign In</h2>
@@ -10,7 +20,7 @@ export default function UserSignIn() {
                 <input id="emailAddress" name="emailAddress" type="email" defaultValue="" />
                 <label htmlFor="password">Password</label>
                 <input id="password" name="password" type="password" defaultValue="" />
-                <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button className="button" type="submit" onClick={handleSubmit}>Sign In</button><button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
             <p>Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!</p>
         </div>

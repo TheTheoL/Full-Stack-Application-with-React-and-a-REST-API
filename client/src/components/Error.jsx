119393f7087ee2ebf6066ddddacde786371fab21
Theo L <atlufkin@outlook.com>
2022-03-30 15:28:28 -0400

    “updated”

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -4,7 +4,12 @@ export default class Error extends Component {
 
     render() {
         return (
-            
+            <main>
+                <div class="wrap">
+                    <h2>Error</h2>
+                    <p>Sorry! We just encountered an unexpected error.</p>
+                </div>
+            </main>
         );
     }
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-
-    return (
-        <header>
-            <div className="wrap header--flex">
-                <h1 className="header--logo"><NavLink to="/">Courses</NavLink></h1>
-                <nav>
-                    <ul className="header--signedout">
-                        <li><NavLink to="/signup">Sign Up</NavLink></li>
-                        <li><NavLink to="/signin">Sign In</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
-
+export default class Header extends React.PureComponent {
+    render() {
+        const { context } = this.props;
+        const authUser = context.authenticatedUser;
+        return (
+            <header>
+                <div className="wrap header--flex">
+                    <h1 className="header--logo"><NavLink to="/">Courses</NavLink></h1>
+                    <nav>
+                        {authUser ? (
+                            <ul className="header--signedin">
+                                <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
+                                <li><NavLink to="/signout">Sign Out</NavLink></li>
+                            </ul>
+                        ) : (
+                            <ul className="header--signedout">
+                                <li><NavLink to="/signup">Sign Up</NavLink></li>
+                                <li><NavLink to="/signin">Sign In</NavLink></li>
+                            </ul>
+                        )}
+                    </nav>
+                </div>
+            </header>
+        );
+    }
 }
-
-export default Header; 

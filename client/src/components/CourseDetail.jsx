97c226afa3a663ffa3eab054d6203b937d4619fe
Theo L <atlufkin@outlook.com>
2022-04-06 13:46:09 -0400

    “updated”

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,22 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
+import { Context } from '../Context';
+import { useParams } from 'react-router-dom';
+import { useHistory } from "react-router-dom"
 
-export default function CourseDetail(props) {
 
+export default function CourseDetail({ history }) {
+
+
+    const context = useContext(Context);
+    //per ReactRouter: useParams returns an object of key/value pairs of URL parameters. Use it to access match.params of the current <Route>.
+    let { id } = useParams();
+
+    //state
     const [course, setCourse] = useState('')
+    const [title, setTitle] = useState('');
+    const [estimatedTime, setEstimatedTime] = useState('');
+    const [description, setDescription] = useState('');
+    const [materialsNeeded, setMaterialsNeeded] = useState('');
+    const [errors, setErrors] = useState([]);
 
 
 
     useEffect(() => {
-        axios.get("http://localhost:5000/api/courses/" + props.match.params.id)
+        axios.get(`http://localhost:5000/api/courses/${id}`)
             .then(res => setCourse(res.data)
             )
     }, [])
 
-    function deleteCourse() {
+    function deleteCourse(event) {
+        event.preventDefault();
+        const deletedCourse = { title, estimatedTime, description, materialsNeeded, userId: context.authenticatedUser.id }
 
+        const encodedCredentials = btoa(`${context.authenticatedUser.emailAddress}:${context.authenticatedUser.password}`);
+
+        fetch(`http://localhost:5000/api/courses/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Basic ${encodedCredentials}`
+            },
+            body: JSON.stringify(deletedCourse)
+        })
+            .then(res => {
+                if (res.status === 400) {
+                    res.json()
+                        .then(data => setErrors(data.errors))
+                } else {
+                    history.push('/');
+                }
+
+            })
     }
 
     return (
@@ -24,7 +60,7 @@ export default function CourseDetail(props) {
             <div className="actions--bar">
                 <div className="wrap">
                     <NavLink className="button" to={`/courses/${course.id}/update`}>Update Course</NavLink>
-                    <NavLink className="button" to="#">Delete Course</NavLink>
+                    <NavLink className="button" to="/" onClick={deleteCourse}>Delete Course</NavLink>
                     <NavLink className="button button-secondary" to="/">Return to List</NavLink>
                 </div>
             </div>
--- client/src/components/UpdateCourse.jsx
+++ client/src/components/UpdateCourse.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Context } from '../Context';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+
 
 export default function UpdateCourse({ history }) {
 
@@ -47,16 +47,7 @@ export default function UpdateCourse({ history }) {
             })
     }
 
-    useEffect(() => {
-        axios.get(`http://localhost:5000/api/courses/${id}`)
-            .then(res => {
-                setTitle(res.data.title);
-                setDescription(res.data.description);
-                setEstimatedTime(res.data.estimatedTime);
-                setMaterialsNeeded(res.data.materialsNeeded);
-            })
-            .catch(err => { console.log('Oh no! Something went wrong fetching data', err); })
-    }, [id]);
+
 
 
 

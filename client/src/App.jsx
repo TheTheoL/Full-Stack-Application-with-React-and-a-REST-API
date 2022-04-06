9aea16be1dbfaca16bfe7b230550d3fbf9da543e
Theo L <atlufkin@outlook.com>
2022-04-06 13:05:27 -0400

    “updated”

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={CoursesWithContext} />
-              <Route path="/courses/:id" component={CourseDetailWithContext} />
+              <Route exact path="/courses/:id" component={CourseDetailWithContext} />
               <Route path="/signin" component={UserSignInWithContext} />
               <Route path="/signup" component={UserSignUpWithContext} />
               <Route path="/signout" component={UserSignOutWithContext} />
--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -15,6 +15,10 @@ export default function CourseDetail(props) {
             )
     }, [])
 
+    function deleteCourse() {
+
+    }
+
     return (
         <main>
             <div className="actions--bar">

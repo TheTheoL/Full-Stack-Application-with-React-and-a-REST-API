421419dbd40992b9d5e9d16c57bde46b9a1c673f
Theo L <atlufkin@outlook.com>
2022-04-07 16:23:47 -0400

    “updated”

--- client/src/components/UserSignUp.jsx
+++ client/src/components/UserSignUp.jsx
@@ -119,13 +119,9 @@ export default class UserSignUp extends Component {
         context.data.createUser(user)
             .then(errors => {
                 if (errors.length) {
-                    return <div className="validation--errors">
-                        <h3>Validation Errors</h3>
-                        <ul>
-                            <li>Please provide a value for "Title"</li>
-                            <li>Please provide a value for "Description"</li>
-                        </ul>
-                    </div>
+                    return this.setState({
+                        errors: errors
+                    })
                 } else {
                     context.actions.signIn(emailAddress, password)
                         .then(() => {
